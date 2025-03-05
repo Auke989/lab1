77f2ffc9c9dea954e5c6c2a2d097700b5dfacc7b
auke <auke989@gmail.com>
2025-03-05 14:08:40 +0100

    fix double expected values

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,5 +1,5 @@
-import uuid
-from functools import reduce
+
+from bson import ObjectId
 
 import pymongo
 
@@ -17,31 +17,30 @@ def add(student=None):
         return 'already exists', 409
 
     # Generate a UUID if required by Swagger
-    student_dict["student_id"] = str(mycol.count_documents({}))
-    res = mycol.insert_one(student_dict)
-    print("-============================================")
-    print(student_dict["student_id"])
-    print(res)
-    print("-============================================")
-    return student_dict["student_id"]
+    res = mycol.insert_one(student.to_dict())
+    student.student_id = res.inserted_id
+    print(student.student_id)
+    return str(student.student_id)
 
 
-def get_by_id(student_id=None, subject=None):
-    if not student_id:
-        return "bad request", 400
-    student = mycol.find_one({"student_id": student_id})
+def get_by_id(student_id=None):
+    student = mycol.find_one({"_id": ObjectId(student_id)})
+
     if not student:
         return "not found", 404
-    student["student_id"] = str(student["student_id"])
-    print(student)
+
+    student["_id"] = str(student["_id"])
+
     return student
 
 
 def delete(student_id=None):
-    if not student_id:
-        return "bad request", 400
-    result = mycol.delete_one({"student_id": student_id})
-    if result.deleted_count == 0:
+    student = mycol.find_one({"_id": ObjectId(student_id)})
+
+    if not student:
         return "not found", 404
 
-    return {"message": "Student deleted", "student_id": str(student_id)}
+    student["_id"] = str(student["_id"])
+
+    mycol.delete_one({"_id": ObjectId(student_id)})
+    return student
